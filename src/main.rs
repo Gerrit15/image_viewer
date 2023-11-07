46149f757529f3fea3490219286a1d3079af76db
gerrit <overlorde159@gmail.com>
2023-11-06 23:02:40 -0500

    on the up and up?

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,47 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use eframe::egui::{self};
-use std::fs;
+use std::{fs, path::PathBuf};
+use clap::Parser;
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 
 fn main() {
-    //let path = "/home/gerrit/Downloads/eye-texture.jpg"
-    //    .split("/").last().unwrap()
-    //    .split(".").last().unwrap();
-    //println!("{}", path);
-    let _ = egui_init();   
+    let args = Args::parse();
+    match args.input {
+        Some(x) => {
+            if x.is_file() {
+                println!("SINGLE IMAGE VIEWING IS NOT CURRENTLY PERMITTED. PLEASE TELL ME TO FIX");
+                _ = egui_init("/home/gerrit/Desktop/loading screens/".to_owned(), args.random);
+            }
+            else {
+                _ = egui_init(x.to_string_lossy().into_owned(), args.random);
+            }
+        },
+        None => {
+            _ = egui_init(".".to_owned(), args.random);
+        }
+    };
 }
 
-fn egui_init() -> Result<(), eframe::Error> {
+fn egui_init(base: String, random: bool) -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
         initial_window_size: None,
         ..Default::default()
     };
 
-    let prelim_paths = fs::read_dir("/home/gerrit/Downloads/")
+    let prelim_paths = fs::read_dir(base)
         .unwrap().filter_map(|e| e.ok())
         .map(|e| e.path().to_string_lossy().into_owned())
+        .filter(|x| {
+            match x.split("/").last().unwrap().split(".").last().unwrap() {
+                "jpg" | "png" | "jpeg" => true,
+                _ => false
+            }})
         .collect::<Vec<_>>();
-    //if everything was working, we'd then filter out the unsupported files here
-    //frankly idk what the fuck to do. the lifetimes are being absolutely infuriating
-    let paths = vec![prelim_paths[0].clone()];
+    let mut paths = prelim_paths;
+    if random { paths.shuffle(&mut thread_rng()) }
 
     let framebox = Box::new(MyApp::new(paths));
     eframe::run_native(
@@ -38,16 +55,26 @@ fn egui_init() -> Result<(), eframe::Error> {
     )
 }
 
+#[derive(Parser)]
+struct Args {
+    ///Where to view from
+    input: Option<PathBuf>,
+
+    ///do you want your images randomized
+    #[arg(short, long)]
+    random: bool
+}
+
 struct MyApp<'a> {
     images: Vec<egui::ImageSource<'a>>,
     index: usize
 }
 
 impl<'a> MyApp<'a> {
-    fn new(path: Vec<String>) -> MyApp<'a> {
+    fn new(paths: Vec<String>) -> MyApp<'a> {
         let mut image_sets = vec![];
-        for i in 0..path.len() {
-            image_sets.push(egui::ImageSource::Uri(std::borrow::Cow::Borrowed(path[i].as_str())))
+        for path in paths {
+            image_sets.push(egui::ImageSource::Uri(std::borrow::Cow::Owned("file://".to_owned() + &path.clone())))
         }
         MyApp {
             images: image_sets,
@@ -58,6 +85,10 @@ impl<'a> MyApp<'a> {
         if self.index < self.images.len() - 1 { self.index += 1}
         else { self.index = 0 }
     }
+    fn previous_image(&mut self) {
+        if self.index > 0 { self.index -= 1}
+        else {self.index = self.images.len() - 1}
+    }
 }
 
 impl<'a> eframe::App for MyApp<'a> {
@@ -65,10 +96,19 @@ impl<'a> eframe::App for MyApp<'a> {
         if ctx.input(|i| i.key_pressed(egui::Key::L)) {
             self.next_image();
         }
+        if ctx.input(|i| i.key_pressed(egui::Key::H)) {
+            self.previous_image();
+        }
+        if ctx.input (|i| i.key_pressed(egui::Key::Q) || i.key_pressed(egui::Key::Escape)) {
+            _frame.close()
+        }
 
         egui::CentralPanel::default().show(ctx, |ui| {
             egui::ScrollArea::new([true, true]).show(ui, |ui| {
-                ui.image(self.images[self.index].clone());
+                //yes this is definitely the best way to handle dirs with no images
+                if self.images.len() > 0 {
+                    ui.image(self.images[self.index].clone());
+                } 
             });
         });
     }
